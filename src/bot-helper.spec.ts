a9da9ce259e44c4aa7c7ffa50582f95671247756
Henry <henrypaul.thoma@gmail.com>
2020-08-25 21:18:35 +0200

    Implementing Jestconfig

--- src/bot-helper.spec.ts
+++ src/bot-helper.spec.ts
@@ -1,3 +1,13 @@
-describe('Wochentags Test', () => {
+import {getWochentag} from './bot-helper.func';
 
+describe('Wochentags Test', () => {
+    it.each`
+    date
+    ${'2020-08-25'}
+    `('text', (date) => {
+       jest.mock('moment', () =>{
+           return () => jest.requireActual('moment')(date)
+       })
+        expect(getWochentag()).toBe('Dienstag');
+    })
 })
